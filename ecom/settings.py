cf962d9e2f6bd1ad316583edd7726539326bb5de
MoraisL <viniciusmoraaais@gmail.com>
2024-05-27 22:08:18 -0300

    feat: add referente to cart in settings

--- ecom/settings.py
+++ ecom/settings.py
@@ -65,11 +65,12 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
+
             ],
         },
     },
 ]
-
 WSGI_APPLICATION = 'ecom.wsgi.application'
 
 
