8714eccd86b89d7344859c763f4c282f8ac0af78
MoraisL <viniciusmoraaais@gmail.com>
2024-05-28 14:18:41 -0300

    feat(store, views): add new fucntion update_info for users

--- store/views.py
+++ store/views.py
@@ -24,3 +24,17 @@ def search(request):
 	else:
 		return render(request, "search.html", {})	
 	
+    
+def update_info(request):
+	if request.user.is_authenticated:
+		current_user = Profile.objects.get(user__id=request.user.id)
+		form = UserInfoForm(request.POST or None, instance=current_user)
+
+		if form.is_valid():
+			form.save()
+			messages.success(request, "Your Info Has Been Updated!!")
+			return redirect('home')
+		return render(request, "update_info.html", {'form':form})
+	else:
+		messages.success(request, "You Must Be Logged In To Access That Page!!")
+		return redirect('home')
