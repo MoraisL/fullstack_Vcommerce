bea04c13cf8fdeb0c4bb75d146c87ec57c1db80e
MoraisL <viniciusmoraaais@gmail.com>
2024-05-28 14:27:25 -0300

    feat(store.views): add function login user in views

--- store/views.py
+++ store/views.py
@@ -109,3 +109,26 @@ def home(request):
 
 def about(request):
 	return render(request, 'about.html', {})	
+
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, ("You Have Been Logged In!"))
+			return redirect('home')
+		else:
+			messages.success(request, ("There was an error, please try again..."))
+			return redirect('login')
+
+	else:
+		return render(request, 'login.html', {})
+
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, ("You have been logged out...Thanks for stopping by..."))
+	return redirect('home')
